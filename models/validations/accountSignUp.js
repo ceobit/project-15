211c499bd3a3064682b0f857559a2df3eb2b8e73
Roman Lebedenko <ceobit90@gmail.com>
2020-05-18 23:51:00 +0200

    after review #3

--- models/validations/accountSignUp.js
+++ models/validations/accountSignUp.js
@@ -1,4 +1,5 @@
 const { Joi } = require('celebrate');
+const validator = require('validator');
 
 const accountSignUp = {
   body: Joi.object().keys({
@@ -7,7 +8,12 @@ const accountSignUp = {
     about: Joi.string()
       .required().min(2).max(30),
     avatar: Joi.string()
-      .required().uri(),
+      .required().custom((value, helpers) => {
+        if (validator.isURL(value)) {
+          return value;
+        }
+        return helpers.message('Ссылка имеет не верный формат');
+      }),
     email: Joi.string()
       .required().email(),
     password: Joi.string()
